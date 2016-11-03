c75937c12650de17fc8dd8f6d44db71176d81dfe
jack19519cv <105021059@live.asia.edu.tw>
2016-11-03 09:05:26 +0800

    hw 繳交

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,28 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int v1  = scn.nextInt();
+		int type =scn.nextInt();
+		System.out.println(fun(v1,type));
 	}
-
-}
+	public static double fun(double v1,double type){
+		Scanner scn = new Scanner(System.in);
+		double	C =1;
+		if(type==1){
+				C = (v1-32)*5/9;
+		}
+		if(type==2){
+				C = v1*9/5+32;
+		}
+		if(type!=1 && type!=2){
+			System.out.println("¿ù»~");
+		}
+		return C;
+	}
+	}
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,26 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("輸入男(1)女(2)後輸入身高:");
+		double v1  = scn.nextInt();
+		double type =scn.nextInt();
+		System.out.println(TALL(type, v1));
 	}
-
+	public static double TALL(double v1,double type){
+		Scanner scn = new Scanner(System.in);
+		double	C =1;
+		if(type==1){
+				C = (v1-170)*0.6+62 ;
+		}else if(type==2){
+				C = (v1-158)*0.5+52;
+		}else{
+			System.out.println("錯誤");
+		}
+		return C;
+	}
 }
